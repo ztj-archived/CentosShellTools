549b28a8f63e500ba499287ee547910a46267bbd
zhangtianjie <ztj1993@gmail.com>
2018-04-17 20:47:09 +0800

    增加了 GoCron 的部署脚本与服务管理脚本

---
+++ shell/deploy/installs/gocron.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+###############
+# Name: 部署安装 GoCron
+# Params 1: Software Version Number
+# author: ZhangTianJie
+# email: ztj1993@gmail.com
+###############
+
+### 引入配置文件
+source /data/shell/funs/loadConfigFile.sh softwares
+
+### 命令行参数
+if [ "${1}" == "help" ]; then
+    echo ">>>>> params 1: SoftwareVersion"
+    exit
+fi
+
+### 变量定义
+Version="${1}"
+[ -z "${Version}" ] && Version="${GoCronVersion}"
+Url="https://github.com/ouqiang/gocron/releases/download/v${Version}/gocron-v${Version}-linux-amd64.tar.gz"
+PackagePath="/data/backups/softwares/gocron-v${Version}-linux-amd64.tar.gz"
+InstallPath="/data/bin/gocron"
+
+### 判断软件是否已经安装
+File="/data/bin/gocron/gocron"
+[ -f "${File}" ] && echo ">>>>> Error: The software has been installed" && exit 1
+
+### 安装前置软件
+
+### 判断软件是否具备安装条件
+
+### 安装软件
+/data/shell/funs/wget.sh ${Url}
+[ $? -ne 0 ] && exit 1
+mkdir -p ${InstallPath}
+tar -xf ${PackagePath} --strip-components 1 -C ${InstallPath}
+chmod 755 /data/bin/gocron/gocron
+
+### 验证软件安装
+/data/bin/gocron/gocron -v > /dev/null 2>&1
+[ $? -ne 0 ] && echo ">>>>> Error: software install fail" && exit 1
+
+### 配置系统
+id -u gocron > /dev/null 2>&1
+[ $? -ne 0 ] && adduser gocron
+
+### 配置软件
+chown -R gocron.gocron ${InstallPath}
+
+### 设置开机启动项
+
+### 其他处理
+
+exit 0
---
+++ shell/service/GoCronNode.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+###############
+# Name: GoCronNode service management
+# Params 1: start|stop|restart
+# author: ZhangTianJie
+# email: ztj1993@gmail.com
+###############
+
+### 定义脚本名称
+ShellName="GoCronNode"
+
+### 定义帮助文本
+if [ "${1}" == "help" ] || [ "${1}" == "" ]; then
+    echo ">>> params 1 <Command>(start|stop|restart)"
+    exit
+fi
+
+### 定义变量
+Command=${1}
+ServiceBin="/data/bin/gocron/gocron-node"
+ServiceLog="/data/logs/service/${ShellName}.`date +%Y-%m-%d-%H-%M-%S`.log"
+Pid=`pgrep -f "${ServiceBin}"`
+
+### 判断变量
+if [ ! -f "${ServiceBin}" ]; then
+    echo ">>>>> Error: the var <ServiceBin> does not exist"
+    exit 0
+fi
+
+### 定义方法
+function start(){
+    [ -n "${Pid}" ] && echo ">>>>> Warning: Service has been started" && return 1
+    /bin/su - gocron -c "${ServiceBin} &" &> ${ServiceLog}
+}
+function stop(){
+    [ -n "${Pid}" ] && kill -9 ${Pid} && Pid=""
+}
+function restart(){
+    stop
+    sleep 3
+    start
+}
+
+### 执行方法
+if [ "$(type -t ${Command})" = "function" ] ; then
+    ${Command}
+else
+    echo ">>>>> Error: the var <Command> does not exist"
+fi

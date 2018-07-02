e97cf75ada48b0e676e5e53a6782737843aa2e4f
zhangtianjie <ztj1993@gmail.com>
2018-07-02 22:47:15 +0800

    优化了 tarDatabaseBackup

--- shell/manual/tarDatabaseBackup.sh
+++ shell/manual/tarDatabaseBackup.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-#
-# 数据库压缩备份
-#
+###############
+# Name: 数据库压缩备份
+# author: ZhangTianJie
+# email: ztj1993@gmail.com
+###############
 
-if [ "${1}" == "help" ]; then
-    echo "本脚本有【二】个参数，分别为：【要备份的数据库名】，【放置备份文件的路径】"
+### 定义帮助文本
+if [ "${1}" == "help" ] || [ "${1}" == "" ]; then
+    echo ">>> params 1 <DbName>"
+    echo ">>> params 2 <SavePath>(DirPath)"
     exit
 fi
-if [ "${1}" == "" ]; then
-    echo ">>>>> Error: 请输入【要备份的数据库名】"
-    exit
+
+### 设置变量
+DbName="${1}"
+SavePath="${2}"
+
+if [ "${DbName}" == "" ]; then
+    echo ">>>>> Error: the var <DbName> does not exist"
+    exit 0
 fi
-if [ "${2}" == "" ]; then
-    echo ">>>>> Error: 请输入【放置备份文件的路径】"
-    exit
+if [ "${SavePath}" == "" ]; then
+    echo ">>>>> Error: the var <SavePath> does not exist"
+    exit 0
 fi
 
 #获取当前系统时间
@@ -26,4 +35,4 @@ source /data/shell/funs/loadConfigFile.sh mysql
 #备份数据库
 mysqldump --host="${db_host}" --port="${db_port}" \
     --user="${db_root_user}" --password="${db_root_pass}" \
-    "${1}" | gzip > "${2}/${1}.${datetime}.sql.gz"
+    "${DbName}" | gzip > "${SavePath}/${DbName}.${datetime}.sql.gz"

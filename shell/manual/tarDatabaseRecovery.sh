5dc3a7a6eb72a169f42afd3e291899326fbebaee
zhangtianjie <ztj1993@gmail.com>
2018-07-02 23:02:06 +0800

    优化了 tarDatabaseRecovery

--- shell/manual/tarDatabaseRecovery.sh
+++ shell/manual/tarDatabaseRecovery.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
-#
-# 数据库压缩恢复
-#
+###############
+# Name: 数据库压缩包恢复
+# author: ZhangTianJie
+# email: ztj1993@gmail.com
+###############
 
-if [ "${1}" == "help" ]; then
-    echo "本脚本有【二】个参数，分别为：【要恢复的数据库名】，【备份文件的路径】"
+### 定义帮助文本
+if [ "${1}" == "help" ] || [ "${1}" == "" ]; then
+    echo ">>> params 1 <DbName>"
+    echo ">>> params 2 <FilePath>"
     exit
 fi
-if [ "${1}" == "" ]; then
-    echo ">>>>> Error: 请输入【要恢复的数据库名】"
-    exit
+
+### 设置变量
+DbName="${1}"
+FilePath="${2}"
+
+### 判断变量
+if [ "${DbName}" == "" ]; then
+    echo ">>>>> Error: the var <DbName> does not exist"
+    exit 0
 fi
-if [ "${2}" == "" ]; then
-    echo ">>>>> Error: 请输入【备份文件的路径】"
-    exit
+if [ "${FilePath}" == "" ]; then
+    echo ">>>>> Error: the var <FilePath> does not exist"
+    exit 0
 fi
 
 #判断数据库文件是否存在
-if [ ! -f "${2}" ]; then
-    echo ">>>>> Error: 【备份文件的路径】不存在，请检查"
-    exit
+if [ ! -f "${FilePath}" ]; then
+    echo ">>>>> Error: the <FilePath> does not exist"
+    exit 0
 fi
 
 #引入数据库配置文件
 source /data/shell/funs/loadConfigFile.sh mysql
 
 #导入数据库文件
-gunzip < ${2} | mysql --host=${db_host} --port=${db_port} \
+gunzip < ${FilePath} | mysql --host=${db_host} --port=${db_port} \
     --user=${db_root_user} --password=${db_root_pass} \
-    --database=${1}
+    --database=${DbName}

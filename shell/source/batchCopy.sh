68038f1b0184028d1060b787a4237ecde2e2bd2d
ZhangTianJie <ztj1993@gmail.com>
2018-07-21 15:04:40 +0800

    重构了 批量复制脚本

--- shell/source/batchCopy.sh
+++ shell/source/batchCopy.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 ###############
-# Name: 批量复制文件
-# Params: <BatchCopyPaths> 复制的路径, TargetPath=OriginalPath
+# Name: 批量复制
+# Params 1: <BatchCopyPaths>(ArrayString) 复制的路径
 # author: ZhangTianJie
 # email: ztj1993@gmail.com
 ###############
 
-### 使用示例
-# declare -A BatchCopyPaths
-# BatchCopyPaths=(["${TargetPath}"]="${OriginalPath}")
+### 定义帮助文本
+if [ "${1}" == "help" ] || [ "${1}" == "" ]; then
+    echo ">>> params 1 <BatchCopyPaths>(ArrayString)[OriginalPath=>TargetPath]"
+    exit
+fi
+
+### 设置变量
+[ -z "${BatchCopyPaths}" ] && BatchCopyPaths=(${*})
 
 ### 判断是否定义了路径
 if [ "${BatchCopyPaths[*]}" == "" ]; then
-    echo ">>>>> Error: var {BatchCopyPaths} does not exist"
+    echo ">>>>> Error: the var <BatchCopyPaths>(ArrayString) does not exist"
     exit 1
 fi
 
 ### 循环数组
-for TargetPath in ${!BatchCopyPaths[@]}
+for Path in ${BatchCopyPaths[@]}
 do
     ### 复制文件
-    \cp -fR "${BatchCopyPaths[${TargetPath}]}" "${TargetPath}"
+    \cp -fR ${Path%=>*} ${Path#*=>}
     [ $? -ne 0 ] && echo ">>>>> Error: copy path error" && exit 1
 done

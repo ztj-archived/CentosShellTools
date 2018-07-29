038f24d6f0b8f686ea20a7edc6a43af54991504f
ZhangTianJie <ztj1993@gmail.com>
2018-07-29 22:07:52 +0800

    优化了 wget 下载脚本

--- shell/funs/wget.sh
+++ shell/funs/wget.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
+###############
+# Name: 恢复备份
+# Params 1: <SoftwareUrl>(Url) 软件地址
+# Params 2: <SoftwarePath>(FilePath) 软件本地路径
+# Params 2: <SoftwareName>(FileName) 软件名称
+# author: ZhangTianJie
+# email: ztj1993@gmail.com
+###############
+
+### 定义帮助文本
+if [ "${1}" == "help" ]; then
+    echo ">>> params 1 <SoftwareUrl>(Url)"
+    echo ">>> params 2 <SoftwarePath>(FilePath)"
+    echo ">>> params 3 <SoftwareName>(FileName)"
+    exit
+fi
+
 ### 判断判断是否存在
-/data/shell/funs/yumInstallSoftware.sh wget yes
-[ $? -ne 0 ] && exit 1
+wget  -V > /dev/null 2>&1
+[ $? -ne 0 ] && echo ">>>>> Error: Please install wget first" && exit 1
 
 ### 变量定义
-SoftwareUrl=${1}
-SoftwareName=${SoftwareUrl##*/}
-SoftwarePath="/data/backups/packages/${SoftwareName}"
-SoftwareLog="/data/logs/deploy/${SoftwareName}.`date +%Y-%m-%d-%H-%M-%S`.log"
+[ -z "${SoftwareUrl}" ] && SoftwareUrl=${1}
+[ -z "${SoftwarePath}" ] && SoftwarePath=${2}
+[ -z "${SoftwareName}" ] && SoftwareName=${3}
+
+[ -z "${SoftwareName}" ] && SoftwareName=${SoftwareUrl##*/}
+[ -z "${SoftwarePath}" ] && SoftwarePath=/data/backups/packages/${SoftwareName}
+
+mkdir -p /data/logs/wget
+SoftwareLog="/data/logs/wget/${SoftwareName}.`date +%Y-%m-%d-%H-%M-%S`.log"
 
 ### 软件不存在时下载文件
 if [ ! -f "${SoftwarePath}" ]; then
@@ -20,5 +42,3 @@ if [ ! -f "${SoftwarePath}" ]; then
         exit 1
     fi
 fi
-
-exit 0

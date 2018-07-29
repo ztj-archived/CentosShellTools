4526237869468c6e2c4cf2095b3040aaa0205684
ZhangTianJie <ztj1993@gmail.com>
2018-07-29 19:35:56 +0800

    优化了 软件安装模板文件

--- shell/software/install/0.template.sh
+++ shell/software/install/0.template.sh
@@ -8,34 +8,34 @@
 
 ### 判断软件是否已经安装
 software -v > /dev/null 2>&1
-[ $? -eq 0 ] && echo ">>>>> Error: The software has been installed" && exit 1
+[ $? -eq 0 ] && echo ">>>>> Error: ${Sign} - The software has been installed" && exit 1
 
-[ -f "${SoftwareInstallPath}" ] && echo ">>>>> Error: The software has been installed" && exit 1
-
-[ -d "${SoftwareInstallPath}" ] && echo ">>>>> Error: The software has been installed" && exit 1
+[ -d "${SoftwareInstallPath}" ] && echo ">>>>> Error: ${Sign} - The software has been installed" && exit 1
 
 ### 安装前置软件
 /data/shell/funs/yumInstallSoftware.sh unzip
-[ $? -ne 0 ] && echo ">>>>> Error: Prefix software install error" && exit 1
+[ $? -ne 0 ] && echo ">>>>> Error: ${Sign} - Prefix software install error" && exit 1
 
 ### 判断软件是否具备安装条件
 software -v > /dev/null 2>&1
-[ $? -ne 0 ] && echo ">>>>> Error: Please install the <java> first" && exit 1
-[ $? -ne 0 ] && echo ">>>>> Error: Do not have installation conditions" && exit 1
+[ $? -ne 0 ] && echo ">>>>> Error: ${Sign} - Please install the <java> first" && exit 1
+[ $? -ne 0 ] && echo ">>>>> Error: ${Sign} - Do not have installation conditions" && exit 1
 
 ### 安装软件
 /data/shell/funs/yumInstallSoftware.sh httpd
-[ $? -ne 0 ] && echo ">>>>> Error: Software install error" && exit 1
+[ $? -ne 0 ] && echo ">>>>> Error: ${Sign} - Software install error" && exit 1
+
 /data/shell/funs/wget.sh ${SoftwareUrl}
-[ $? -ne 0 ] && echo ">>>>> Error: Software download error" && exit 1
+[ $? -ne 0 ] && echo ">>>>> Error: ${Sign} - Software download error" && exit 1
+
 mkdir -p ${SoftwareInstallPath}
 unzip ${SoftwarePackagePath} -d ${SoftwareInstallPath}
 tar -xf ${PackagePath} --strip-components 1 -C ${SoftwareInstallPath}
-[ $? -ne 0 ] && echo ">>>>> Error: Software unzip|untar error" && exit 1
+[ $? -ne 0 ] && echo ">>>>> Error: ${Sign} - Software unzip|untar error" && exit 1
 
 ### 验证软件安装
 software -v > /dev/null 2>&1
-[ $? -ne 0 ] && echo ">>>>> Error: Software install fail" && exit 1
+[ $? -ne 0 ] && echo ">>>>> Error: ${Sign} - Software install fail" && exit 1
 
 ### 配置系统
 
